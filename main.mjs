bca7f9acc0d847dba7d977986660eb494fe862a7
j0code <jonaslp759@gmail.com>
2022-04-13 19:24:37 +0200

    mysql - users table to keep track of users and when they last drew - canvas table to keep track of canvas changes

--- main.mjs
+++ main.mjs
@@ -1,11 +1,14 @@
 import fs from "fs"
 import https from "https"
 import express from "express";
+import * as sqllib from "mysql"
 import config from "./config-loader.mjs"
 
 const app = express();
 let httpsServer;
 if(config.usehttps) httpsServer = https.createServer(config.https, app);
+
+const sql = sqllib.createConnection(config.sql)
 
 app.use(express.static("htdocs")); // only for static files that dont change
 
@@ -22,3 +25,13 @@ if(config.usehttps) {
 		console.log("Server is running on http://localhost:" + config.ports.http + "/");
 	});
 }
+
+sql.connect(e => {
+	if(e) {
+		console.error(e)
+		process.kill(1) // exit if connect fails
+	}
+
+	sql.query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20) NOT NULL, creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, lastdraw DATETIME)") // users table
+	sql.query("CREATE TABLE canvas (id INT NOT NULL, x SMALLINT NOT NULL, y SMALLINT NOT NULL, date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, color CHAR(6) NOT NULL DEFAULT \"ffffff\")") // canvas table
+})
