459a8aa9ff08c99c629c26603cef8d3410ac41e1
Cfp <cfp@cfp.gotdns.ch>
2022-04-13 18:04:29 +0200

    Use config-loader.mjs

--- main.mjs
+++ main.mjs
@@ -1,10 +1,11 @@
 import fs from "fs"
 import https from "https"
 import express from "express";
+import config from "./config-loader.mjs"
 
 const app = express();
 let httpsServer;
-if(USEHTTPS) httpsServer = https.createServer({cert: CERTPATH,key: KEYPATH}, app);
+if(config.usehttps) httpsServer = https.createServer(config.https, app);
 
 app.use(express.static("public")); // only for static files that dont change
 
@@ -12,16 +13,16 @@ app.use(express.static("public")); // only for static files that dont change
 // 	res.send("Hello world!");
 // })
 
-if(USEHTTPS) {
+if(config.usehttps) {
 	// TODO: Add http redirect here
 
-	httpsServer.listen(HTTPSPORT, () => {
-		console.log("HTTPS Server is running on http://localhost:" + HTTPSPORT + "/");
+	httpsServer.listen(config.ports.https, () => {
+		console.log("HTTPS Server is running on http://localhost:" + config.ports.https + "/");
 	});
 } else {
 	console.warn("==== WARNING: HTTPS IS NOT ENABLED! ====");
 	console.warn("IT IS HIGHLY RECOMMENDED TO ENABLE HTTPS");
-	app.listen(PORT, () => {
-		console.log("Server is running on http://localhost:" + PORT + "/");
+	app.listen(config.ports.http, () => {
+		console.log("Server is running on http://localhost:" + config.ports.http + "/");
 	});
 }
