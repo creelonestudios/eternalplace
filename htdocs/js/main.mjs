cbad665af5942261f0ac75bd93ce259843b931ce
j0code <jonaslp759@gmail.com>
2022-04-13 22:49:40 +0200

    add canvas - draw grid (currently just gradient) - draw border around selected pixel

--- htdocs/js/main.mjs
+++ htdocs/js/main.mjs
@@ -1,3 +1,42 @@
 import API from "./api.mjs"
+import { $ } from "./util.mjs"
 
-API.test("reddituser1", 3, 4, "123456")
+// will be polled from server later
+const width  = 20
+const height = 20
+
+const canvas = $("#place")
+
+const mouse = {
+	x: 0, y: 0
+}
+
+function draw() {
+	console.log("draw!")
+	canvas.width = canvas.clientWidth
+	canvas.height = canvas.clientHeight
+	let ctx = canvas.getContext("2d")
+	ctx.fillStyle = "#000000"
+	ctx.fillRect(0, 0, canvas.width, canvas.height)
+	for(let i = 0; i < width; i++) {
+		for(let j = 0; j < height; j++) {
+			ctx.fillStyle = `rgb(${i*255/20}, 0, ${j*255/20})`
+			ctx.fillRect(i*40, j*40, 40, 40)
+		}
+	}
+	let x = Math.floor(mouse.x /width /2) *40
+	let y = Math.floor(mouse.y /height /2) *40
+	ctx.strokeStyle = `#ffffff`
+	ctx.beginPath();
+	ctx.rect(x-0.5, y-0.5, 40 +1, 40 +1);
+	ctx.stroke();
+}
+
+requestAnimationFrame(draw)
+
+window.addEventListener("resize", () => requestAnimationFrame(draw))
+canvas.addEventListener("mousemove", e => {
+	mouse.x = e.offsetX
+	mouse.y = e.offsetY
+	requestAnimationFrame(draw)
+})
---
+++ htdocs/js/util.mjs
@@ -0,0 +1,41 @@
+export function $(q) {
+	return document.querySelector(q)
+}
+
+export function e(q) {
+	var q = cssQuery(q)
+	if(!q.tag) return null
+	var el = document.createElement(q.tag)
+	if(q.id) el.id = q.id
+	if(q.className) el.className = q.className
+	if(q.attrs) for(var a in q.attrs) el[a] = q.attrs[a]
+	return el
+}
+
+// thanks to Denys Séguret on https://stackoverflow.com/questions/17888039/javascript-efficient-parsing-of-css-selector
+// slightly altered
+function cssQuery(q) {
+  var obj = {tag: "", classes: [], id: "", attrs: {}, className: ""}
+  if(q) q.split(/(?=\.)|(?=#)|(?=\[)/).forEach(token => {
+    switch (token[0]) {
+      case '#':
+        obj.id = token.slice(1)
+        break
+      case '.':
+        obj.classes.push(token.slice(1))
+        break;
+      case '[':
+				var a = token.slice(1,-1).split(';')
+        for(var b of a) {
+          var kv = b.split("=")
+  				obj.attrs[kv[0]] = kv[1]
+        }
+        break
+      default:
+        obj.tag = token
+        break
+    }
+  })
+	obj.className = obj.classes.join(" ")
+  return obj
+}
