b4d64b85283f3ea956732967790d3239bbe3f496
Cfp <cfp@cfp.gotdns.ch>
2022-04-13 17:58:35 +0200

    Express server and stuff like that

--- main.mjs
+++ main.mjs
@@ -1,2 +1,34 @@
 import fs from "fs"
 import https from "https"
+import express from "express";
+
+// CONFIG
+const PORT = 8080;
+const HTTPSPORT = 8443;
+const USEHTTPS = true; // highly recommended
+const CERTPATH = "";
+const KEYPATH = "";
+
+const app = express();
+let httpsServer;
+if(USEHTTPS) httpsServer = https.createServer({cert: CERTPATH,key: KEYPATH}, app);
+
+app.use(express.static("public")); // only for static files that dont change
+
+// app.get("/", (req, res) => {
+// 	res.send("Hello world!");
+// })
+
+if(USEHTTPS) {
+	// TODO: Add http redirect here
+
+	httpsServer.listen(HTTPSPORT, () => {
+		console.log("HTTPS Server is running on http://localhost:" + HTTPSPORT + "/");
+	});
+} else {
+	console.warn("==== WARNING: HTTPS IS NOT ENABLED! ====");
+	console.warn("IT IS HIGHLY RECOMMENDED TO ENABLE HTTPS");
+	app.listen(PORT, () => {
+		console.log("Server is running on http://localhost:" + PORT + "/");
+	});
+}
